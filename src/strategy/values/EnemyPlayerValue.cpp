965d300203b7753558090d39f7f18906163d1124
bashermens <31279994+hermensbas@users.noreply.github.com>
2026-01-11 01:52:25 +0100

    [HOTFIX] Revert "Fix/Feat: Stop bots in party from PVP when master isn't, and … (#2003)
    …PVP probablity system (#1914)"
    
    This reverts commit 02e8465a3b91a12ca2ce56a1a704dfd80368c121.
    
    
    [worldserver_gdb_20260111_000520.log](https://github.com/user-attachments/files/24547263/worldserver_gdb_20260111_000520.log)
    
    
    Noticed the server kept crashing all the sudden, first thought it was
    local issue but see dump.

--- src/strategy/values/EnemyPlayerValue.cpp
+++ src/strategy/values/EnemyPlayerValue.cpp
@@ -11,10 +11,6 @@
 
 bool NearestEnemyPlayersValue::AcceptUnit(Unit* unit)
 {
-    // Apply parent's filtering first (includes level difference checks)
-    if (!PossibleTargetsValue::AcceptUnit(unit))
-        return false;
-
     bool inCannon = botAI->IsInVehicle(false, true);
     Player* enemy = dynamic_cast<Player*>(unit);
     if (enemy && botAI->IsOpposing(enemy) && enemy->IsPvP() &&
@@ -23,14 +19,7 @@ bool NearestEnemyPlayersValue::AcceptUnit(Unit* unit)
         ((inCannon || !enemy->HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE))) &&
         /*!enemy->HasStealthAura() && !enemy->HasInvisibilityAura()*/ enemy->CanSeeOrDetect(bot) &&
         !(enemy->HasSpiritOfRedemptionAura()))
-    {
-        // If with master, only attack if master is PvP flagged
-        Player* master = botAI->GetMaster();
-        if (master && !master->IsPvP() && !master->IsFFAPvP())
-            return false;
-
         return true;
-    }
 
     return false;
 }
