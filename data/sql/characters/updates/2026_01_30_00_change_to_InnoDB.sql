3c62a45fad0827b0d0279e9f92f18ab05f9ede4a
bash <hermensb@gmail.com>
2026-02-15 20:47:26 +0100

    Revert "Convert PlayerBots tables to InnoDB (#2083)"
    This reverts commit c86032f43b7bccb861c33769385db7a522e75513.

--- data/sql/characters/updates/2026_01_30_00_change_to_InnoDB.sql
+++
@@ -1,9 +0,0 @@
--- Temporarily disables innodb_strict_mode for the session to allow the script to complete even if legacy table definitions contain InnoDB-incompatible attributes
-SET SESSION innodb_strict_mode = 0;	
-
--- Change the tables to InnoDB
-ALTER TABLE playerbots_guild_names ENGINE=InnoDB;
-ALTER TABLE playerbots_names ENGINE=InnoDB;
-
--- Re-enables innodb_strict_mode 
-SET SESSION innodb_strict_mode = 1;
